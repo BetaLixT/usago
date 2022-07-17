0a1ecf7f5819ba1b6b8a1474da7c610ae990bd85
BetaLixT <39497438+BetaLixT@users.noreply.github.com>
2022-07-17 09:01:51 +0400

    tests on consumer

--- manager_test.go
+++ manager_test.go
@@ -141,6 +141,122 @@ func TestPublishAck(t *testing.T) {
 	manager.Close()
 }
 
+// I'm just using this to test locally while killing and restarting the
+// rabbitmq server
+func TestConsumer(t *testing.T) {
+	lgr, _ := zap.NewDevelopment()
+	manager := NewChannelManager(
+		"amqp://guest:guest@localhost:5672/",
+		lgr,
+	)
+
+	bldr := NewChannelBuilder().WithQueue(
+		"hello1",
+		false,
+		false,
+		false,
+		false,
+		nil,
+	).WithConfirms(true)
+	chnl, err := manager.NewChannel(*bldr)
+	if err != nil {
+		lgr.Error(
+			"failed to create channel",
+			zap.Error(err),
+		)
+		t.FailNow()
+	}
+	messageCount := 10
+	cnfrms, err := chnl.GetConfirmsChannel()
+	if err != nil {
+		lgr.Error(
+			"failed to get confirms channel",
+			zap.Error(err),
+		)
+		t.FailNow()
+	}
+
+	wg := sync.WaitGroup{}
+	
+	wg.Add(2)
+	go func() {
+		defer wg.Done()
+		for i := 0; i < messageCount; i++ {
+			ack := <-cnfrms
+			lgr.Info("confirm recieved")
+			if ack.Ack {
+			} else {
+				lgr.Error("failed delivery")
+			}
+		}
+	}()
+	count := 0
+	consumer, err := chnl.RegisterConsumer(
+		"hello1",
+		"",
+		true,
+		false,
+		false,
+		false,
+		nil,
+	)
+	if err != nil {
+		lgr.Error(
+			"failed to register consumer",
+			zap.Error(err),
+		)
+		t.FailNow()
+	}
+	go func () {
+		defer wg.Done()
+
+		for i := 0; i < messageCount; i++ {
+			msg := <-consumer
+			lgr.Info(
+				"message read",
+				zap.String("body", string(msg.Body)),
+			)
+			count++
+		}
+	}()
+
+	body := "Hello World!"
+	for i := 0; i < messageCount; i++ {
+		lgr.Info("publishing...")
+		_, err = chnl.Publish(
+			"",
+			"hello1",
+			false, // mandatory
+			false, // immediate
+			amqp.Publishing{
+				ContentType: "text/plain",
+				Body:        []byte(body),
+			},
+		)
+		for err != nil {
+			_, err = chnl.Publish(
+				"",
+				"hello1",
+				false, // mandatory
+				false, // immediate
+				amqp.Publishing{
+					ContentType: "text/plain",
+					Body:        []byte(body),
+				},
+			)
+		}
+	}
+	wg.Wait()
+	if count != messageCount {
+		lgr.Error(
+			"message count miss match",
+			zap.Int("count", count),
+			zap.Int("messageCount", messageCount),
+		)
+	}
+	manager.Close()
+}
+
 func TestDelayedPub(t *testing.T) {
 	lgr, _ := zap.NewDevelopment()
 	manager := NewChannelManager(
@@ -192,8 +308,8 @@ func TestDelayedPub(t *testing.T) {
 
 	body := "Hello World!"
 	for i := 0; i < messageCount; i++ {
-	  time.Sleep(5 * time.Second)
-	  lgr.Info("publishing...")
+		time.Sleep(5 * time.Second)
+		lgr.Info("publishing...")
 		_, err = chnl.Publish(
 			"",
 			"hello",
